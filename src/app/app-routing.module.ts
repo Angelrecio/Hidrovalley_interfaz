b245ac6384cbb9239365d106545b436ca32598e8
Ángel Mejía Recio <22052179@live.uem.es>
2023-06-03 15:16:31 +0200

    Cambio icono, login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppbarComponent } from './appbar/appbar.component';
 import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
+  {path: "", redirectTo: "/path_login", pathMatch: "full"},
   {path: "path_appbar", component:AppbarComponent},
   {path: "path_login", component:LoginComponent},
+  {path: "path_registro", component:RegistroComponent},
 ];
 
 @NgModule({
